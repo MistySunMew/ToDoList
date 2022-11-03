cd0e6ec26e5369f4fc91749cff6feea56525eb60
Michael Forbes <michael.e.forbes@comcast.net>
2022-11-03 10:13:04 -0700

    Basic add ToDo implemented

--- js/main.js
+++ js/main.js
@@ -1,14 +1,48 @@
 var picker = datepicker("#due-date");
 picker.setMin(new Date());
 var ToDoItem = (function () {
-    function ToDoItem(title) {
-        this.title = title;
+    function ToDoItem() {
     }
     return ToDoItem;
 }());
+window.onload = function () {
+    var addItem = $("add");
+    addItem.onclick = main;
+};
+function main() {
+    if (isValid()) {
+        var item = getToDoItem();
+        displayToDoItem(item);
+    }
+}
 function isValid() {
+    return true;
 }
 function getToDoItem() {
+    var myItem = new ToDoItem();
+    myItem.task = $("task").value;
+    myItem.dueDate = new Date($("due-date").value);
+    myItem.isCompleted = $("task").checked;
+    return myItem;
 }
 function displayToDoItem(item) {
+    var itemText = document.createElement("h3");
+    itemText.innerText = item.task;
+    var itemDate = document.createElement("p");
+    itemDate.innerText = item.dueDate.toDateString();
+    var itemDiv = document.createElement("div");
+    if (item.isCompleted) {
+        itemDiv.classList.add("completed");
+    }
+    itemDiv.appendChild(itemText);
+    itemDiv.appendChild(itemDate);
+    if (item.isCompleted) {
+        $("complete-items").appendChild(itemDiv);
+    }
+    else {
+        $("incomplete-items").appendChild(itemDiv);
+    }
+}
+function $(id) {
+    return document.getElementById(id);
 }
--- ts/main.ts
+++ ts/main.ts
@@ -3,12 +3,20 @@ const picker = datepicker("#due-date");
 picker.setMin(new Date()); //Set to today's date
 
 class ToDoItem {
-    title:string;
+    task:string;
     dueDate:Date;
     isCompleted:boolean;
+}
 
-    constructor(title:string) {
-        this.title = title;
+window.onload = function() {
+    let addItem = $("add");
+    addItem.onclick = main;
+}
+
+function main() {
+    if(isValid()) {
+        let item = getToDoItem();
+        displayToDoItem(item);
     }
 }
 
@@ -16,21 +24,58 @@ class ToDoItem {
  * Check form data is valid
  */
 function isValid():boolean {
-
+    return true;
 }
 
 /**
  * Gets data from the form and constructs a ToDoItem object
  */
 function getToDoItem():ToDoItem {
+    let myItem = new ToDoItem();
 
+    //get task
+    myItem.task = (<HTMLInputElement>$("task")).value;
+
+    //get due date
+    myItem.dueDate = new Date((<HTMLInputElement>$("due-date")).value);
+
+    //get isCompleted
+    myItem.isCompleted = (<HTMLInputElement>$("task")).checked;
+
+    return myItem;
 }
 
 /**
  * Display given ToDoItem on the webpage
  */
 function displayToDoItem(item:ToDoItem):void {
+    //Creates a <h3> element and sets the text to the task name
+    let itemText = document.createElement("h3")
+    itemText.innerText = item.task;
 
+    //Creates a <p> element and sets the text to the dueDate
+    let itemDate = document.createElement("p")
+    itemDate.innerText = item.dueDate.toDateString();
+
+    //Creates a <div> element and if the ToDoItem is marked completed adds a "completed" class
+    let itemDiv = document.createElement("div");
+    if (item.isCompleted) {
+        itemDiv.classList.add("completed");
+    }
+
+    itemDiv.appendChild(itemText);
+    itemDiv.appendChild(itemDate);
+
+    //Adds itemDiv to the webpage based on completion
+    if (item.isCompleted) {
+        $("complete-items").appendChild(itemDiv);
+    }
+    else {
+        $("incomplete-items").appendChild(itemDiv);
+    }
 }
 
+function $(id):HTMLElement {
+    return document.getElementById(id);
+}
 //TODO: Allow user to mark a ToDoItem as completed
