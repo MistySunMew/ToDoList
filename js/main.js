114c1b839de934ef2dc7318aca7077cd98703158
Michael Forbes <michael.e.forbes@comcast.net>
2022-11-03 09:54:23 -0700

    Added outline for functions

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 var picker = datepicker("#due-date");
 picker.setMin(new Date());
+var ToDoItem = (function () {
+    function ToDoItem(title) {
+        this.title = title;
+    }
+    return ToDoItem;
+}());
+function isValid() {
+}
+function getToDoItem() {
+}
+function displayToDoItem(item) {
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,2 +1,36 @@
+//@ts-ignore: Ignoring issue with js-datepicker lack of intellisense
 const picker = datepicker("#due-date");
 picker.setMin(new Date()); //Set to today's date
+
+class ToDoItem {
+    title:string;
+    dueDate:Date;
+    isCompleted:boolean;
+
+    constructor(title:string) {
+        this.title = title;
+    }
+}
+
+/**
+ * Check form data is valid
+ */
+function isValid():boolean {
+
+}
+
+/**
+ * Gets data from the form and constructs a ToDoItem object
+ */
+function getToDoItem():ToDoItem {
+
+}
+
+/**
+ * Display given ToDoItem on the webpage
+ */
+function displayToDoItem(item:ToDoItem):void {
+
+}
+
+//TODO: Allow user to mark a ToDoItem as completed
